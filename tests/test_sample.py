c66307fb9fc55478eb733dfc5b2b8ee540dcc3f0
Sharanya Senthil <61621049+Sharanya142000@users.noreply.github.com>
2024-06-06 18:39:30 -0400

    test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,40 +1,8 @@
 import pytest
 import pandas as pd
 import numpy as np
-from pathlib import Path
+from dags.utils.data_preprocessing import util_data_preprocessing
 
-# Define your preprocessing function
-def util_data_preprocessing(df):
-    columns_drop = {'SBP', 'DBP', 'EtCO2', 'BaseExcess', 'HCO3', 'pH', 'PaCO2', 
-                    'Alkalinephos', 'Calcium', 'Magnesium', 'Phosphate', 'Potassium', 
-                    'PTT', 'Fibrinogen', 'Unit1', 'Unit2'}
-    df["Unit"] = df["Unit1"] + df["Unit2"]
-
-    # dropping columns based on redundancy
-    df.drop(columns=columns_drop, inplace=True)
-    grouped_by_patient = df.groupby('Patient_ID')
-
-    # imputing backfill and forward fill
-    df = grouped_by_patient.apply(lambda x: x.bfill().ffill())
-
-    # dropping all the columns with null values more than 25% and patient_id
-    null_columns = ['TroponinI', 'Bilirubin_direct', 'AST', 'Bilirubin_total', 
-                    'Lactate', 'SaO2', 'FiO2', 'Unit', 'Patient_ID']
-    df = df.drop(columns=null_columns)
-
-    # gaussian transformation
-    columns_normalized = ['MAP', 'BUN', 'Creatinine', 'Glucose', 'WBC', 'Platelets']
-    for i in columns_normalized:
-        df[i] = np.log(df[i] + 1)
-
-    # onehot encoding gender column
-    one_hot = pd.get_dummies(df['Gender'])
-    df = df.join(one_hot)
-    df = df.drop('Gender', axis=1)
-    df = df.dropna()
-    return df
-
-# Define the fixture to create a dummy DataFrame
 @pytest.fixture
 def input_dataframe():
     data = {
@@ -73,7 +41,6 @@ def input_dataframe():
     df = pd.DataFrame(data)
     return df
 
-# Define the test functions using the fixture
 def test_column_dropping(input_dataframe):
     df = util_data_preprocessing(input_dataframe.copy())
     columns_dropped = {'SBP', 'DBP', 'EtCO2', 'BaseExcess', 'HCO3', 'pH', 'PaCO2', 
