7e66f61bdabc63d76f63814241853b5f8e3920e7
deril2605 <derilraju@gmail.com>
2024-06-13 13:31:43 -0400

    vertex-train-job

--- dags/dag_model_data_and_store.py
+++ dags/dag_model_data_and_store.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
+from airflow.providers.google.cloud.operators.vertex_ai.custom_job import CreateCustomContainerTrainingJobOperator
 
 
 default_args = {
@@ -19,7 +20,16 @@ with DAG(
     template_searchpath=["/opt/airflow/dags/utils"]
 ) as dag:
 
-    task_dummy_start = DummyOperator(task_id='start_task')
+    create_custom_container_training_job = CreateCustomContainerTrainingJobOperator(
+        task_id="train_sepsis_on_vertex_ai",
+        staging_bucket=f"sepsis-prediction-outputs/vertexai/train_joboutputs-{str(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))}",
+        display_name=f"train_sepsis_on_vertex_ai",
+        container_uri="us-central1-docker.pkg.dev/leafy-sunrise-425218-h4/sepsis-mlops-repo/train_pipeline:latest",
+        replica_count=1,
+        machine_type="n1-standard-4",
+        region="us-central1",
+        args=['--gcs_bucket_path=sepsis-prediction-mlops/data/processed_data']
+    )
     task_dummy_end = DummyOperator(task_id='end_task')
 
-    task_dummy_start >> task_dummy_end
+    create_custom_container_training_job >> task_dummy_end
