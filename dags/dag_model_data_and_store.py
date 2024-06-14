d43bb2b9f178760e2ebc8a3ea9589e2e761b90b3
deril2605 <derilraju@gmail.com>
2024-06-14 00:19:30 -0400

    better model output location

--- dags/dag_model_data_and_store.py
+++ dags/dag_model_data_and_store.py
@@ -28,13 +28,12 @@ with DAG(
 
     create_custom_container_training_job = CreateCustomContainerTrainingJobOperator(
         task_id="train_sepsis_on_vertex_ai",
-        staging_bucket=f"sepsis-prediction-outputs/vertexai/train_joboutputs-{str(datetime.now().strftime('%Y%m%d-%H%M%S'))}",
         display_name=f"train_sepsis_on_vertex_ai",
         container_uri="us-central1-docker.pkg.dev/leafy-sunrise-425218-h4/sepsis-mlops-repo/train_pipeline:latest",
         replica_count=1,
         machine_type="n1-standard-4",
         region="us-central1",
-        args=['--gcs_bucket_path=sepsis-prediction-mlops/data/processed_data'],
+        args=['--gcs_bucket_path=sepsis-prediction-mlops/data/processed_data','--model_dir=gs://sepsis-prediction-mlops/models'],
         gcp_conn_id=config.GCP_CONN_ID
     )
     task_dummy_end = DummyOperator(task_id='end_task')
