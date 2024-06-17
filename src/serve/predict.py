9636f7159d4839dc15bb03500f1dfe040a380682
deril2605 <derilraju@gmail.com>
2024-06-16 20:59:56 -0400

    logging for train and serve

--- src/serve/predict.py
+++ src/serve/predict.py
@@ -22,7 +22,7 @@ table_id = os.environ['MODEL_MONITORING_TABLE_ID']
 
 client = logging.Client()
 logger = client.logger('Serving_Pipeline')
-logger.log_text(f'Iniatiating Pipeline {datetime.now().isoformat()}', severity='INFO')
+logger.log_text(f'Iniatiating Serving Pipeline {datetime.now().isoformat()}', severity='INFO')
 
 def create_logging_table_schema():
     """Build the table schema for the logging table
@@ -51,9 +51,11 @@ def create_or_get_logging_table_bq(client, table_id):
     except Exception as e:
         print(e)
         print(f"Table {table_id} not found. Creating table...")
+        logger.log_text(f"Table {table_id} not found. Creating table...", severity='INFO')
         schema = create_logging_table_schema()
         table = bigquery.Table(table_id, schema)
         client.create_table(table)
+        logger.log_text(f"Created table {table.project}.{table.dataset_id}.{table.table_id}", severity='INFO')
         print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
 
 def initialize_client_and_bucket(bucket_name=bucket_name):
@@ -82,6 +84,7 @@ def load_pickle_from_bucket(bucket, pickle_file_path):
 def load_scaler(bucket, pickle_file_path='artifacts/scaler.pkl'):
     scaler = load_pickle_from_bucket(bucket, pickle_file_path)
     print("Downloaded Scaler")
+    logger.log_text("Downloaded Scaler", severity='INFO')
     return scaler
 
 def load_model(bucket, models_prefix='models'):
@@ -95,90 +98,83 @@ def load_model(bucket, models_prefix='models'):
             model_folders[timestamp] = blob.name.split('/')[1]  # Extract folder name
 
     if not model_folders:
+        logger.log_text("No model folders found in the specified bucket and prefix.", severity='ERROR')
         raise Exception("No model folders found in the specified bucket and prefix.")
 
     latest_model_folder = model_folders[max(model_folders.keys())]
+    logger.log_text(f"Latest Model: {latest_model_folder}", severity='INFO')
     print("Latest Model: ", latest_model_folder)
     
     model_dir = f'models/{latest_model_folder}/model.pkl'
     print("Model Directory: ", model_dir)
+    logger.log_text(f"Model Directory: {model_dir}", severity='INFO')
     model = load_pickle_from_bucket(bucket, model_dir)
     print("Loaded Model")
+    logger.log_text(f"Loaded Model", severity='INFO')
 
     return model
 
 def data_preprocess_pipeline(features):
-    if True:
-        print(features.shape)
-        df = pd.DataFrame(features, columns=['HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'EtCO2',
-       'BaseExcess', 'HCO3', 'FiO2', 'pH', 'PaCO2', 'SaO2', 'AST', 'BUN',
-       'Alkalinephos', 'Calcium', 'Chloride', 'Creatinine', 'Bilirubin_direct',
-       'Glucose', 'Lactate', 'Magnesium', 'Phosphate', 'Potassium',
-       'Bilirubin_total', 'TroponinI', 'Hct', 'Hgb', 'PTT', 'WBC',
-       'Fibrinogen', 'Platelets', 'Age', 'Gender', 'Unit1', 'Unit2',
-       'HospAdmTime', 'ICULOS', 'Patient_ID'])
-        columns_to_drop = ['SBP', 'DBP', 'EtCO2', 'BaseExcess', 'HCO3',
-                           'pH', 'PaCO2', 'Alkalinephos', 'Calcium',
-                           'Magnesium', 'Phosphate', 'Potassium', 'PTT',
-                           'Fibrinogen', 'Unit1', 'Unit2']
-        df["Unit"] = df["Unit1"] + df["Unit2"]
-        df.drop(columns=columns_to_drop, inplace=True)
+    df = pd.DataFrame(features, columns=['HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'EtCO2',
+    'BaseExcess', 'HCO3', 'FiO2', 'pH', 'PaCO2', 'SaO2', 'AST', 'BUN',
+    'Alkalinephos', 'Calcium', 'Chloride', 'Creatinine', 'Bilirubin_direct',
+    'Glucose', 'Lactate', 'Magnesium', 'Phosphate', 'Potassium',
+    'Bilirubin_total', 'TroponinI', 'Hct', 'Hgb', 'PTT', 'WBC',
+    'Fibrinogen', 'Platelets', 'Age', 'Gender', 'Unit1', 'Unit2',
+    'HospAdmTime', 'ICULOS', 'Patient_ID'])
+    columns_to_drop = ['SBP', 'DBP', 'EtCO2', 'BaseExcess', 'HCO3',
+                        'pH', 'PaCO2', 'Alkalinephos', 'Calcium',
+                        'Magnesium', 'Phosphate', 'Potassium', 'PTT',
+                        'Fibrinogen', 'Unit1', 'Unit2']
+    df["Unit"] = df["Unit1"] + df["Unit2"]
+    df.drop(columns=columns_to_drop, inplace=True)
 
+
+
+    grouped_by_patient = df.groupby('Patient_ID')
+
+    # Impute missing values with forward and backward fill
+    df = grouped_by_patient.apply(lambda x: x.bfill().ffill()).reset_index(drop=True)
+
+    # Drop columns with more than 25% null values and 'Patient_ID'
+    columns_with_nulls = ['TroponinI', 'Bilirubin_direct', 'AST', 'Bilirubin_total',
+                            'Lactate', 'SaO2', 'FiO2', 'Unit', 'Patient_ID']
+    df.drop(columns=columns_with_nulls, inplace=True)
+
+    # Apply log transformation to normalize specific columns
+    columns_to_normalize = ['MAP', 'BUN', 'Creatinine', 'Glucose', 'WBC', 'Platelets']
+    for col in columns_to_normalize:
+        if df[col].notna().all():
+            print(df[col].values)
+            df[col] = np.log1p(df[col])
+
+
+    # One-hot encode the 'Gender' column
     
+    df['gender'] = df['Gender'].replace({1: 'M', 0: 'F'}).apply(lambda x: x if x in ['M', 'F'] else np.nan)
+    
+    
+    encoded_gender_col = pd.get_dummies(df["gender"])
+    
+    if 'F' not in encoded_gender_col:
+        encoded_gender_col['F'] = 0
+    if 'M' not in encoded_gender_col:
+        encoded_gender_col['M'] = 0
 
-        grouped_by_patient = df.groupby('Patient_ID')
+    df = df.join(encoded_gender_col)
+    df = df.drop(columns=["Gender", "gender"], axis=1)
 
-        # Impute missing values with forward and backward fill
-        df = grouped_by_patient.apply(lambda x: x.bfill().ffill()).reset_index(drop=True)
+    df['F'] = df['F'].astype(int)
+    df['M'] = df['M'].astype(int)
+    # Drop remaining rows with any NaN values
+    df.dropna(inplace=True)
 
-        # Drop columns with more than 25% null values and 'Patient_ID'
-        columns_with_nulls = ['TroponinI', 'Bilirubin_direct', 'AST', 'Bilirubin_total',
-                              'Lactate', 'SaO2', 'FiO2', 'Unit', 'Patient_ID']
-        df.drop(columns=columns_with_nulls, inplace=True)
+    # Split the dataframe back into X and y
+    X_preprocessed = df.reset_index(drop=True)
+    
+    X_preprocessed = X_preprocessed.drop(columns=X_preprocessed.columns[X_preprocessed.columns.str.contains('^Unnamed', case=False, regex=True)])
 
-        # Apply log transformation to normalize specific columns
-        columns_to_normalize = ['MAP', 'BUN', 'Creatinine', 'Glucose', 'WBC', 'Platelets']
-        for col in columns_to_normalize:
-            if df[col].notna().all():
-                print(df[col].values)
-                df[col] = np.log1p(df[col])
-
-
-        # One-hot encode the 'Gender' column
-        
-        df['gender'] = df['Gender'].replace({1: 'M', 0: 'F'}).apply(lambda x: x if x in ['M', 'F'] else np.nan)
-        
-        
-        encoded_gender_col = pd.get_dummies(df["gender"])
-        
-        if 'F' not in encoded_gender_col:
-            encoded_gender_col['F'] = 0
-        if 'M' not in encoded_gender_col:
-            encoded_gender_col['M'] = 0
-
-        df = df.join(encoded_gender_col)
-        df = df.drop(columns=["Gender", "gender"], axis=1)
-
-        df['F'] = df['F'].astype(int)
-        df['M'] = df['M'].astype(int)
-        # Drop remaining rows with any NaN values
-        df.dropna(inplace=True)
-
-        # Split the dataframe back into X and y
-        X_preprocessed = df.reset_index(drop=True)
-        
-        X_preprocessed = X_preprocessed.drop(columns=X_preprocessed.columns[X_preprocessed.columns.str.contains('^Unnamed', case=False, regex=True)])
-
-        return X_preprocessed
-        
-    # except KeyError as ke:
-    #     print("KeyError during preprocessing: %s", ke)
-    # except ValueError as ve:
-    #     print("ValueError during preprocessing: %s", ve)
-    # except Exception as ex:
-    #     print("An unexpected error occurred during preprocessing: %s", ex)
-    #     raise
-
+    return X_preprocessed
 
 @app.route(os.environ['AIP_HEALTH_ROUTE'], methods=['GET'])
 def health_check():
@@ -211,6 +207,7 @@ def predict():
     if input_array.ndim == 1:
         input_array = input_array.reshape(1, -1)
     print(input_array.shape)
+    logger.log_text(f"Array Shape: {input_array.shape}", severity='INFO')
     if input_array.shape[1]!= 41:
         return jsonify({"error": "Invalid input shape"}), 400
     features = input_array.copy()
@@ -230,6 +227,10 @@ def predict():
         "average_latency_per_row":prediction_latency / len(input_array),
     }]
     errors = bq_client.insert_rows_json(table_id, log_latency_data)
+    if errors == []:
+        logger.log_text("New predictions inserted into Logging table BQ.", severity='INFO')
+    else:
+        logger.log_text(f"Encountered errors inserting predictions into Logging table BQ.: {errors}", severity='ERROR')
 
     return jsonify({"predictions": predictions.tolist()})
 
