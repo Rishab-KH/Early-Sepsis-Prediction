7fde79beada319d5c89a97baec9e1531fd9e94a2
Sharanya Senthil <senseesha@gmail.com>
2024-06-25 18:04:08 -0400

    attempt to create count_df

--- src/data_stratifier/startify_dataset.py
+++ src/data_stratifier/startify_dataset.py
@@ -21,3 +21,10 @@ def read_and_concat_psv_files(folder):
     # Concatenate all DataFrames
     combined_df = pd.concat(all_dataframes, ignore_index=True)
     return combined_df
+
+def create_count_df(combined_df):
+    # Count occurrences of SepsisLabel for each PID
+    count_0 = combined_df[combined_df['SepsisLabel'] == 0].groupby('PID').size().reset_index(name='count_of_0')
+    count_1 = combined_df[combined_df['SepsisLabel'] == 1].groupby('PID').size().reset_index(name='count_of_1')
+
+
