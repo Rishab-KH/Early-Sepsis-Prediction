5df949820e2b3a8dc68f29994ee951632c1b4e37
Sharanya Senthil <senseesha@gmail.com>
2024-06-25 19:36:41 -0400

    set page config

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -8,3 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 streamlit_uri = os.getenv('streamlit_uri')
+
+
+def main():
+    st.set_page_config(page_title="EMR Sepsis Prediction System", layout="wide")
+
+if __name__ == "__main__":
+    main()
