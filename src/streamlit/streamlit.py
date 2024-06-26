5b1482592f7ddc7072a2afcd97704c65da0d3913
Rishab <rishabkhuba3108@gmail.com>
2024-06-26 06:36:36 -0400

    visualize pred in streamlit

--- src/streamlit/streamlit.py
+++ src/streamlit/streamlit.py
@@ -47,7 +47,7 @@ def main():
             predictions = response.json().get("predictions")
             
             st.subheader("Predictions:")
-            # st.table(predictions)  # Use st.table for better visualization
+            st.table(predictions)  # Use st.table for better visualization remove later
             
             # Check if any prediction contains "1"
             for pred in predictions:
